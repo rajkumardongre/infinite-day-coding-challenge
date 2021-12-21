e86a9075f961d7fd03af866c858f42ba9f5587fa
rajkumardongre <rajkumardongre17@gmail.com>
2021-12-21 21:21:03 +0530

    day 1

---
+++ c++_program/factorial_of_number.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int factorial(int n){
+    if(n==0){
+        return 1;
+    }
+    return n*factorial(n-1);
+}
+
+int main(){
+    int n;
+    cout<<"Enter Your Number : ";
+    cin>>n;
+    cout<<factorial(n);
+
+
+    return 0;
+}

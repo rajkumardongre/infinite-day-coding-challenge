c50b31b7f6eb6573937e58d0e6e27b52302253ac
rajkumardongre <rajkumardongre17@gmail.com>
2021-12-21 20:05:30 +0530

    day 1

--- c++_program/prime_factor.cpp
+++ c++_program/prime_factor.cpp
@@ -1,17 +1,29 @@
 #include<iostream>
 using namespace std;
-int main(){
-    // 50 => 2,5,5 | 20 => 2,2,5
-    int n;
-    cout<<"Enter number : ";
-    cin>>n;
-    for(int i=2; n>i; i++){
-        if(n%i == 0){
-            for(int j = 1; i>j; j++){
-                if(i%j==0 && j!=1){
-                    cout<<", "<<j;
-                }
-            }
+
+bool isPrime(int n){
+    for(int i = 2; i < n; i++){
+        if(n%i==0){
+            return false;
         }
     }
+    return true;
+}
+
+void primeFactor(int n){
+    for(int i=2; i<=n; i++){
+        if(n%i==0 && isPrime(i)){
+            cout<<i<<" ";
+            return primeFactor(n/i);
+        }
+    }
+}
+
+int main(){
+    // 50 => 2,5,5 | 20 => 2,2,5 | 13 => 13
+    int n;
+    // int nm;
+    cout<<"Enter Number : ";
+    cin>>n;
+    primeFactor(n);
 }

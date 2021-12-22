9e5b3effb115a76ec9b42f3885fc2b2c85fd7bed
rajkumardongre <rajkumardongre17@gmail.com>
2021-12-22 10:51:46 +0530

    Day 2

---
+++ c++_program/prime_number_next_to_n.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+bool isPrime(int n){
+    for(int i = 2; i<n; i++){
+        if(n%i==0){
+            return false;
+        }
+    }
+    return true;
+}
+
+int nextPrimeNo(int n){
+    for(int i=n+1; ;i++){
+        if(isPrime(i)){
+            return i;
+        }
+    }
+}
+
+int main(){
+    // cout<<"Hello World";
+    cout<<nextPrimeNo(7);
+
+    return 0;
+}

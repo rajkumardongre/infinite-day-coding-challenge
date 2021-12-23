ada1d1dac23889f5a06fe2e8866a3325ed4614d0
rajkumardongre <rajkumardongre17@gmail.com>
2021-12-23 13:02:17 +0530

    day 2

---
+++ c++_program/pointer/pointer_vs_arrays.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int arr[] = {10, 20, 30};
+    int *ptr = arr;
+    // cout<<*ptr<<endl;
+    // cout<<ptr<<endl;
+    // cout<<*arr<<endl;
+    // cout<<sizeof(ptr)<<endl;
+    // cout<<sizeof(*ptr)<<endl;
+    // cout<<sizeof(arr)<<endl;
+    cout<<(arr[3]);
+
+    return 0;
+}

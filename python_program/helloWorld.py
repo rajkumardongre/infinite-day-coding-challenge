bc3b8fa9d9e01d74cd1a7b3134b9aace973543ef
rajkumardongre <rajkumardongre17@gmail.com>
2021-12-24 00:00:46 +0530

    day 3

---
+++ python_program/helloWorld.py
@@ -0,0 +1,21 @@
+print("Hello World")
+# SWAPING 
+a=10
+b=20
+print("a => ",a,"b => ",b)
+a,b=b,a
+print("a => ",a,"b => ",b)
+
+# id() METHOD
+print(id(5))
+a=10
+b=10
+c=a
+print(id(a))
+print(id(b))
+print(id(c))
+# is operator will check wether its operends refering to same memory or not
+# return True if referring to same memory else return false
+print(a is b)
+print(a is c)
+print(a is 10)

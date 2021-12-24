b9ebffc3e822c68a3c7b86b39839ee09ff688706
rajkumardongre <rajkumardongre17@gmail.com>
2021-12-24 10:58:52 +0530

    day 4

---
+++ python_program/even_odd.py
@@ -0,0 +1,41 @@
+# n = 9
+# print("Number is Even :",end=" ")
+# if n%2:
+#     print(False)
+# else:
+#     print(True)
+    
+# positive negative zero
+# n = -0
+# if(n>0):
+#     print("Positive")
+# elif(n==0):
+#     print("Zero")
+# else:
+#     print("Negative")
+    
+# sign and form
+# n = -0
+# if(n>0):
+#     print("Positive", end=" ")
+#     if(n%2==0):
+#         print("Even")
+#     else:
+#         print("Odd")
+# elif(n<0):
+#     print("Negative", end=" ")
+#     if(n%2==0):
+#         print("Even")
+#     else:
+#         print("Odd")
+# else:
+#     print("Zero")
+
+# a and b
+a,b = 5,5
+if(a>b):
+    print("a is greater")
+elif(a<b):
+    print("b is greater")
+else:
+    print("a and b both are same")

fe7a071afc54d93acffff75a5db79afb37ea7d19
rajkumardongre <rajkumardongre17@gmail.com>
2021-12-24 15:08:26 +0530

    day 4

--- python_program/for_loop.py
+++ python_program/for_loop.py
@@ -10,8 +10,15 @@
 #         break 
 
 # avoid multiple of 5
-l = [2,5,2,6,10,6,8,15,8]
-for i in l:
-    if(i%5==0):
-        continue
-    print(i, end=" ")
+# l = [2,5,2,6,10,6,8,15,8]
+# for i in l:
+#     if(i%5==0):
+#         continue
+#     print(i, end=" ")
+    
+# Tables from 1 to 10
+for i in range(1,11):
+    for j in range(1,11):
+        print(i*j,end="   ")
+    print()
+    
--- python_program/tempCodeRunnerFile.py
+++ python_program/tempCodeRunnerFile.py
@@ -1,10 +1,6 @@
-# Rule 1 : Multiple of 4, but not multiple of 100
-# Rule 2 : Multiple of 400
-
-year = 2100
-if(year%4==0 and year%100!=0):
-        print("leap year")
-elif(year%400==0):
-    print("leap year")
-else:
-    print("Not Leap year")
+= [2,5,2,6,10,6,8,15,8]
+# for i in l:
+#     if(i%5==0):
+#         continue
+#     print(i, end=" ")
+   

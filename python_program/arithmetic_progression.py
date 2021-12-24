b3a77a5fce065779e1e223d05f6cdaac66e81a5e
rajkumardongre <rajkumardongre17@gmail.com>
2021-12-24 09:15:43 +0530

    day 4

---
+++ python_program/arithmetic_progression.py
@@ -0,0 +1,7 @@
+init_amount = 5000
+inc_per_month = 2000
+time_in_year = 5
+total_time = 12*time_in_year
+
+res = init_amount + total_time*inc_per_month
+print(res)
---
+++ python_program/input_output.py
@@ -0,0 +1,11 @@
+# output
+print("Welcome to", end=" ")
+print("the World")
+
+print("24", "12", "2021", sep="-")
+print(24, "02", 2021, sep="-")
+
+# input
+name = input("Enter your name : ")
+print("welcome ", name)
+print(type(name))
